fb64cc013633740946e6f6e36dec8d75d45607b0
Richard Hsieh <richardhsieh1@hotmail.com>
2017-02-13 20:01:18 +0000

    First commit of modular HTML

---
+++ getAndPrintHTML2.js
@@ -0,0 +1,28 @@
+var https = require('https');
+function getAndPrintHTML (options) {
+  var str = ''
+  https.get(options, function (response) {
+
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
+
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      str = str + data;
+      console.log('Chunk Received. Output', data.length);
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+      console.log('Response stream complete.', str);
+    });
+  });
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
